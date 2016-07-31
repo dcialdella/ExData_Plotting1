c06869645b4dbb40334e1ca9b260cc13ffb5e60f
dcialdella <dcialdella@gmail.com>
2016-07-31 20:14:56 +0200

    plot3.r

--- plot3.r
+++ plot3.r
@@ -1,5 +1,3 @@
-
-
 # read input file - very larg + 2m records
 inputfile <- "./data/household_power_consumption.txt"
 
@@ -13,7 +11,8 @@ subGrupo <- datos[datos$Date %in% c("1/2/2007","2/2/2007") ,]
 datetime     <- strptime(paste(subGrupo$Date, subGrupo$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 
 # GAP = Global_active_power
-SolutionGAP  <- as.numeric(subGrupo$Global_active_power)
+# SolutionGAP  <- as.numeric(subGrupo$Global_active_power)
+
 subPartes1 <- as.numeric(subGrupo$Sub_metering_1)
 subPartes2 <- as.numeric(subGrupo$Sub_metering_2)
 subPartes3 <- as.numeric(subGrupo$Sub_metering_3)
@@ -22,7 +21,7 @@ subPartes3 <- as.numeric(subGrupo$Sub_metering_3)
 png("plot3.png", width=480, height=480)
 
 # Graphic subparte 1
-plot(datetime,  subPartes1, type="l", ylab="Energy Submetering", xlab="")
+plot(datetime,  subPartes1, type="l", ylab="Energy Submetering.", xlab="")
 # overgraph parte2
 lines(datetime, subPartes2, type="l", col="red")
 # overgraph parte 3
